13de6decccd18acc33ef8cd5d581b474ff800d22
Elisha Witte <elishawitte@gmail.com>
2017-03-02 15:00:22 +0100

    User Authentication

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Authentication Views
+// @TODO: Replace with our own views.
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+
+// Default Laravel Authentication Routes
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+$this->post('register', 'Auth\RegisterController@register');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
+
+
