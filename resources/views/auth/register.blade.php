d9d1f82112487fb0469bae2c13e018261d97e930
Bernhard Kelm <bern.kelm@gmail.com>
2017-03-03 17:52:30 +0100

    added the styles for the login page, small changes to the register page style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,39 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="form_center_wrapper">
-        <div id="form_center">
-            <h2>{{ config('app.name', 'Laravel') }}</h2>
-                <form role="form" id="register" method="POST" action="{{ route('register') }}">
-                    {{ csrf_field() }}
-                    <span>Sign up! It's free.</span>
-                    <input placeholder="Name" type="text" name="name" value="{{ old('name') }}" required autofocus>
-                    @if ($errors->has('name'))
-                        <span class="help-block">
-                            {{ $errors->first('name') }}
-                        </span>
-                    @endif
-                    <div class="radio_wrapper">
-                        <input type="radio" name="role" value="professional"><label>Professional</label>
-                        <input type="radio" name="role" value="company"><label>Company</label>
-                    </div>
-                    <input placeholder="Email" type="email" name="email" value="{{ old('email') }}" required>
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                            {{ $errors->first('email') }}
-                        </span>
-                    @endif
-                    <input placeholder="Password" type="password" name="password" required>
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                            {{ $errors->first('password') }}
-                        </span>
-                    @endif
-                    <input type="password" placeholder="Confirm passowrd" name="password_confirmation" required>
-                    <button type="submit" class="btn btn-primary">
-                        Register
-                    </button>
-                </form>
-        </div>
+<div id="form_center_wrapper">
+    <div id="form_center">
+        <h2>{{ config('app.name', 'Laravel') }}</h2>
+        <form role="form" id="register" method="POST" action="{{ route('register') }}">
+            {{ csrf_field() }}
+            <span>Sign up! It's free.</span>
+            <input placeholder="Name" type="text" name="name" value="{{ old('name') }}" required autofocus>
+            @if ($errors->has('name'))
+                <span class="help-block">
+                    {{ $errors->first('name') }}
+                </span>
+            @endif
+            <div class="radio_wrapper">
+                <input type="radio" name="role" value="professional"><label>Professional</label>
+                <input type="radio" name="role" value="company"><label>Company</label>
+            </div>
+            <input placeholder="Email" type="email" name="email" value="{{ old('email') }}" required>
+            @if ($errors->has('email'))
+                <span class="help-block">
+                    {{ $errors->first('email') }}
+                </span>
+            @endif
+            <input placeholder="Password" type="password" name="password" required>
+            @if ($errors->has('password'))
+                <span class="help-block">
+                    {{ $errors->first('password') }}
+                </span>
+            @endif
+            <input type="password" placeholder="Confirm passowrd" name="password_confirmation" required>
+            <button type="submit" class="btn btn-primary">
+                Register
+            </button>
+            </form>
     </div>
+</div>
 @endsection
