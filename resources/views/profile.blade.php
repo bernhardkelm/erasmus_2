315bcb40f6a35ffe79c5d3f4546ebd898d9ebf7c
Elisha Witte <elishawitte@gmail.com>
2017-03-07 17:23:42 +0100

    Fixed profile tabs.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -76,114 +76,114 @@
             </div>
         </div>
     </div>
-    <div class="col_right has-depth">
+    <div class="col_right">
         <div class="tab_wrapper">
-            <div name="profile_tab" class="tab active">Profil</div>
+            <div name="profile_tab" class="tab active">Feed</div>
             <div name="requests_tab" class="tab right">Job Requests</div>
         </div>
 
         <div class="profile_wrapper">
+            <form role="form" id="store_post" name="store_post" method="POST" action="{{ route('posts.store') }}">
+                {{ csrf_field() }}
+                <textarea name="body" placeholder="Write a message..."></textarea>
+                <a class="button is-outline is-animated"
+                   href="javascript:{}" onclick="document.getElementById('store_post').submit();">
+                    <span>Submit</span>
+                    <span class="icon">
+                        <i class="mdi mdi-chevron-right"></i>
+                    </span>
+                </a>
+            </form>
 
-        <form role="form" id="store_post" name="store_post" method="POST" action="{{ route('posts.store') }}">
-            {{ csrf_field() }}
-            <textarea name="body" placeholder="Write a message..."></textarea>
-            <a class="button is-outline is-animated"
-               href="javascript:{}" onclick="document.getElementById('store_post').submit();">
-                <span>Submit</span>
-                <span class="icon">
-                    <i class="mdi mdi-chevron-right"></i>
-                </span>
-            </a>
-        </form>
+            <hr />
 
-        <hr />
-        <div class="feed">
-            <h4>User Feed</h4>            
-            @foreach ($posts as $post)
-                <div class="row">
-                    <div class="column">
-                    <div class="post">
-                        <div class="post__image">
-                            @if($user->picture)
-                                <img class="img" src="{{ $user->picture }}">
-                            @else 
-                                <img class="img" src="{{ asset('images/default_avatar.jpg') }}">
-                            @endif
-                        </div>
-                        <div class="post__content">
-                            <p class="post__user">{{ $user->name }} 
-                                <span class="post__date">{{ $post->getDiffTimeForHumans() }} ago</span>
-                            </p>
-                            <p>{{ $post->body }}</p>
-                            <ul class="post__controls">
-                                @can('update', $post)
-                                    <li><a href="#">Edit</a></li>
-                                @endcan
-                                @can('delete', $post)
-                                    <li><form role="form" method="POST" action="{{ route('posts.destroy', ['id' => $post->id]) }}">
-                                            {{ csrf_field() }}
-                                            {{ method_field('DELETE') }}
-                                            <input type="submit" value="Delete">
-                                        </form></li>
-                                @endcan
-                            </ul>
-                        </div>
-                        <div class="clearfix"></div>
-                    </div>
-                    </div>
-                </div>
-                @foreach ($post->comments as $comment)
+            <div class="feed">
+                <h4>User Feed</h4>
+                @foreach ($posts as $post)
                     <div class="row">
-                        <div class="column is-offset-1">
-                            <div class="comment">
-                                <div class="comment__image">
-                                    @if($comment->user->picture)
-                                        <img class="img" src="{{ $comment->user->picture }}">
-                                    @else 
-                                        <img class="img" src="{{ asset('images/default_avatar.jpg') }}">
-                                    @endif
-                                </div>
-                                <div class="comment__content">
-                                    <p class="comment__user">{{ $comment->user->name }}
-                                        <span class="comment_date">{{ $comment->getDiffTimeForHumans() }} ago</span>
-                                    </p>
-                                    <p>{{ $comment->body }}</p>
-                                    <ul class="comment__controls">
-                                    @can('update', $comment)
+                        <div class="column">
+                        <div class="post">
+                            <div class="post__image">
+                                @if($user->picture)
+                                    <img class="img" src="{{ $user->picture }}">
+                                @else
+                                    <img class="img" src="{{ asset('images/default_avatar.jpg') }}">
+                                @endif
+                            </div>
+                            <div class="post__content">
+                                <p class="post__user">{{ $user->name }}
+                                    <span class="post__date">{{ $post->getDiffTimeForHumans() }} ago</span>
+                                </p>
+                                <p>{{ $post->body }}</p>
+                                <ul class="post__controls">
+                                    @can('update', $post)
                                         <li><a href="#">Edit</a></li>
                                     @endcan
-                                    @can('delete', [$comment, $post])
-                                        <li><form role="form" method="POST" action="{{ route('comments.destroy', ['id' => $comment->id]) }}">
-                                            {{ csrf_field() }}
-                                            {{ method_field('DELETE') }}
-                                            <input type="submit" value="Delete">
-                                        </form></li>
+                                    @can('delete', $post)
+                                        <li><form role="form" method="POST" action="{{ route('posts.destroy', ['id' => $post->id]) }}">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+                                                <input type="submit" value="Delete">
+                                            </form></li>
                                     @endcan
                                 </ul>
-                                </div>
-                                <div class="clearfix"></div>
                             </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        </div>
+                    </div>
+                    @foreach ($post->comments as $comment)
+                        <div class="row">
+                            <div class="column is-offset-1">
+                                <div class="comment">
+                                    <div class="comment__image">
+                                        @if($comment->user->picture)
+                                            <img class="img" src="{{ $comment->user->picture }}">
+                                        @else
+                                            <img class="img" src="{{ asset('images/default_avatar.jpg') }}">
+                                        @endif
+                                    </div>
+                                    <div class="comment__content">
+                                        <p class="comment__user">{{ $comment->user->name }}
+                                            <span class="comment_date">{{ $comment->getDiffTimeForHumans() }} ago</span>
+                                        </p>
+                                        <p>{{ $comment->body }}</p>
+                                        <ul class="comment__controls">
+                                        @can('update', $comment)
+                                            <li><a href="#">Edit</a></li>
+                                        @endcan
+                                        @can('delete', [$comment, $post])
+                                            <li><form role="form" method="POST" action="{{ route('comments.destroy', ['id' => $comment->id]) }}">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+                                                <input type="submit" value="Delete">
+                                            </form></li>
+                                        @endcan
+                                    </ul>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                    <div class="row">
+                        <div class="commentbox column is-offset-1">
+                            <form role="form" method="POST" action="{{ route('comments.store', ['id' => $post->id]) }}">
+                                {{ csrf_field() }}
+                                <textarea name="body" placeholder="Write a comment..."></textarea>
+                                <button type="submit" class="button is-outline">
+                                    <span>Submit</span>
+                                </button>
+                            </form>
                         </div>
                     </div>
                 @endforeach
-                <div class="row">
-                    <div class="commentbox column is-offset-1">
-                        <form role="form" method="POST" action="{{ route('comments.store', ['id' => $post->id]) }}">
-                            {{ csrf_field() }}
-                            <textarea name="body" placeholder="Write a comment..."></textarea>
-                            <button type="submit" class="button is-outline">
-                                <span>Submit</span>
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            @endforeach
-        </div>
+            </div> <!-- div.feed -->
 
-        </div>
+        </div> <!-- div.profile_wrapper -->
 
-    </div>
-</div>
+    </div><!-- div.col_right -->
+</div> <!-- div.container -->
 <div class="clearfix"></div>
 </div>
 @endsection
