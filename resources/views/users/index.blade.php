4230538bb4d2e3816908b31eea39b75528d7c6cf
Bernhard Kelm <bern.kelm@gmail.com>
2017-03-07 17:22:05 +0100

    safety commig

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Users</h1>
-    @foreach($users as $user)
-        <div class="usercard">
-            <h5>{{ $user->name }}</h5>
-            <p>{{ $user->country or '' }} - {{ $user->major or '' }}</p>
-        </div>
-    @endforeach
-    {{ $users->links() }}
+	<h3>Users</h3>
+	@foreach($users as $user)
+    <div class="usercard">
+      <h5>{{ $user->name }}</h5>
+      <p>{{ $user->country or '' }} - {{ $user->major or '' }}</p>
+    </div>
+	@endforeach
+	{{ $users->links() }}
 @endsection
